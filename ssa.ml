326576160c0d1d56c65a21bb25f9df57ce4f4ebb
Hakjoo Oh <hakjoo.oh@gmail.com>
2015-10-19 16:16:19 +0900

    update

---
+++ ssa.ml
@@ -0,0 +1,10 @@
+(* NOTE: This is an optional, challenge problem. 
+ * If you implement the following function and it works correctly, 
+ * then your final grade will be raised up to two steps, e.g., B+ -> A+ *)
+
+(*  TODO: Implement a function that convert a T program into its SSA form.
+ *  You may also need to modify the abstract syntax definition of T 
+ *  to extend it with "phi"-functions. *)
+
+let toSSA : T.program -> T.program
+=fun t -> t 
