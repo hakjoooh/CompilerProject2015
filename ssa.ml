0fccaa591433cecaeb13636cee488ee2e6b1edc5
Hakjoo Oh <hakjoo.oh@gmail.com>
2015-10-19 17:24:11 +0900

    update

--- ssa.ml
+++ ssa.ml
@@ -1,8 +1,4 @@
-(* NOTE: This is an optional, challenge problem. 
- * If you implement the following function and it works correctly, 
- * then your final grade will be raised up to two steps, e.g., B+ -> A+ *)
-
-(*  TODO: Implement a function that convert a T program into its SSA form.
+(*  TODO(optional): Implement a function that convert a T program into its SSA form.
  *  You may also need to modify the abstract syntax definition of T 
  *  to extend it with "phi"-functions. *)
 
